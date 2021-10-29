5fdd5cf7bb052e0725a0c7ea8b6ab00019e009b2
ThomasNight <thomas.michel@nightborn.be>
2021-10-29 11:15:28 +0200

    Update to nextJs 12

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
-import React from "react";
+import { useRef } from "react";
 import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import { AnimateSharedLayout } from "framer-motion";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 function MyApp({ Component, pageProps }) {
-	const queryClientRef = React.useRef();
+	const queryClientRef = useRef();
 	if (!queryClientRef.current) {
 		// @ts-ignore
-		queryClientRef.current = new QueryClient({ defaultOptions: { queries: { refetchIntervalInBackground: true, refetchOnReconnect: false, refetchOnMount: true, refetchOnWindowFocus: false } } });
+		queryClientRef.current = new QueryClient({
+			defaultOptions: {
+				queries: {
+					refetchIntervalInBackground: true,
+					refetchOnReconnect: false,
+					refetchOnMount: true,
+					refetchOnWindowFocus: false,
+				},
+			},
+		});
 	}
 
 	return (
@@ -16,7 +25,10 @@ function MyApp({ Component, pageProps }) {
 			<Head>
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" />
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;200;300;400;500;600;700;800;900&display=swap"
+					rel="stylesheet"
+				/>
 			</Head>
 			<QueryClientProvider client={queryClientRef.current}>
 				<ChakraProvider>
